32154678f2e9b5976aed0130f3c317d02ce29478
Rajesh Radhakrishnan <sendmail2rajesh24@gmail.com>
2024-08-28 13:53:52 +0530

    Update app.test.js

--- src/__test__/app.test.js
+++ src/__test__/app.test.js
@@ -1,1 +1,13 @@
 
+// component/ __test__/App.test.js
+
+
+// import { shallow } from 'enzyme';
+// import App from './App.jsx';
+
+// describe('App', ()=> {
+// it('renders correctly', ()=> {
+// const wrapper = shallow(<App />);
+// expect(wrapper).toMatchSnapshot();
+// });
+// });
